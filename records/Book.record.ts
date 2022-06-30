3ae29d120c5c4f539cbc4b212988467d34ad2b2d
konradmlecz <44980580+konradmlecz@users.noreply.github.com>
2022-06-30 21:01:41 +0200

    feat: clear property isLend

--- records/Book.record.ts
+++ records/Book.record.ts
@@ -7,32 +7,28 @@ export class BookEntity {
   description: string;
   publischedAt: string;
   publisherId: string;
-  isLend: boolean;
   constructor({
     id,
     title,
     description = null,
     publischedAt,
     publisherId,
-    isLend,
   }: BookEntity) {
     this.id = id ?? uuid();
     this.title = title;
     this.description = description;
     this.publischedAt = publischedAt;
     this.publisherId = publisherId;
-    this.isLend = isLend;
   }
   async insertOne() {
     await pool.execute(
-      "INSERT INTO `book`(`id`,`title`, `description`, `publischedAt`, `publisherId`, `isLend`) VALUES (:id,:title,:description,:publischedAt ,:publisherId ,:isLend )",
+      "INSERT INTO `book`(`id`,`title`, `description`, `publischedAt`, `publisherId`) VALUES (:id,:title,:description,:publischedAt ,:publisherId)",
       {
         id: this.id,
         title: this.title,
         description: this.description,
         publischedAt: this.publischedAt,
         publisherId: this.publisherId,
-        isLend: this.isLend,
       }
     );
     return this.id;
@@ -40,7 +36,7 @@ export class BookEntity {
 
   static async getAll() {
     const [bookItems] = await pool.execute(
-      "SELECT `book`.`id`, `book`.`title`, `book`.`description`,`book`.`publischedAt`, `book`.`isLend`, `publisher`.`name` AS publisherName, `author`.`name` AS authorName, `author`.`surname` AS authorSurname FROM `book` JOIN `publisher` ON `book`.`publisherId` = `publisher`.`id` JOIN `book_author` ON `book`.`id` = `book_author`.`bookId` JOIN `author` ON `book_author`.`authorId` = `author`.`id`"
+      "SELECT `book`.`id`, `book`.`title`, `book`.`description`,`book`.`publischedAt`, `publisher`.`name` AS publisherName, `author`.`name` AS authorName, `author`.`surname` AS authorSurname FROM `book` JOIN `publisher` ON `book`.`publisherId` = `publisher`.`id` JOIN `book_author` ON `book`.`id` = `book_author`.`bookId` JOIN `author` ON `book_author`.`authorId` = `author`.`id`"
     );
     return bookItems;
   }
